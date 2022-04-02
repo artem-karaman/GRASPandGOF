a75d47a7b74aeb0f89f287e58c20ce70c371bef8
artem-karaman <karaman55_09@mail.ru>
2022-04-02 13:34:51 +0300

    Add composite implementation

---
+++ GOF/Stuctural design patterns/Composite/Composite/Assets/Scripts/Composite/Component.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Core.Composite
+{
+    internal interface IComponent
+    {
+        public string Name { get; }
+        public string Display();
+        public void Add(IComponent c);
+        public void Remove(IComponent c);
+    }
+}
