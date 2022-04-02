4ee5c0027189c93821e02830b99e1ab3ce65b7eb
artem-karaman <karaman55_09@mail.ru>
2022-04-02 14:48:31 +0300

    clean up IComponent

---
+++ GOF/Stuctural design patterns/Composite/Composite/Assets/Scripts/Composite/IComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Composite
+{
+    internal interface IComponent
+    {
+        string Name { get; }
+        string Display();
+        void Add(IComponent c);
+        void Remove(IComponent c);
+    }
+}
